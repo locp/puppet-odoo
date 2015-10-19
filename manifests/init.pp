ed2e716bfe83cadcd8d728a2de34da3861672240
dallinb <b.dalling@locp.co.uk>
2015-10-19 13:07:10 +0100

    Code refactoring (#2).

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,14 +10,14 @@ class odoo9 {
   package { [ 'fontconfig', 'freetype', 'libpng', 'libjpeg-turbo', 'openssl',
               'libX11', 'libXext', 'libXrender', 'xorg-x11-fonts-Type1',
               'xorg-x11-fonts-75dpi', 'epel-release' ]:
-    ensure  => present,
-    before  => Package['wkhtmltopdf'],
-    notify  => Service['odoo']
+    ensure => present,
+    before => Package['wkhtmltopdf'],
+    notify => Service['odoo']
   }
 
   package { 'wkhtmltopdf':
-    name     => 'wkhtmltox',
     ensure   => 'present',
+    name     => 'wkhtmltox',
     source   => 'http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-centos7-amd64.rpm',
     provider => 'rpm',
     before   => Package['odoo'],
