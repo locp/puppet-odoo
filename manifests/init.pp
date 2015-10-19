5a182c3ecfe800ba0032dc8c6bc4fa6a2cfa27f0
dallinb <b.dalling@locp.co.uk>
2015-10-19 09:02:52 +0100

    Work on the manifest (#2).

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,13 +4,24 @@
 class odoo9 {
   class { 'postgresql::server':
     before => Package['odoo'],
+    notify => Service['odoo']
+  }
+
+  package { [ 'fontconfig', 'freetype', 'libpng', 'libjpeg-turbo', 'openssl',
+              'libX11', 'libXext', 'libXrender', 'xorg-x11-fonts-Type1',
+              'xorg-x11-fonts-75dpi', 'epel-release' ]:
+    ensure  => present,
+    before  => Package['wkhtmltopdf'],
+    notify  => Service['odoo']
   }
 
   package { 'wkhtmltopdf':
-    ensure          => 'present',
-    name            => 'http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-centos7-amd64.rpm',
-    provider        => 'rpm',
-    before          => Package['odoo'],
+    name     => 'wkhtmltox',
+    ensure   => 'present',
+    source   => 'http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-centos7-amd64.rpm',
+    provider => 'rpm',
+    before   => Package['odoo'],
+    notify   => Service['odoo']
   }
 
   yumrepo { 'odoo-nightly':
@@ -21,9 +32,16 @@ class odoo9 {
     gpgcheck => True,
     gpgkey   => 'https://nightly.odoo.com/odoo.key',
     before   => Package['odoo'],
+    notify   => Service['odoo']
   }
 
   package { 'odoo':
     ensure => 'present',
+    notify => Service['odoo']
+  }
+
+  service { 'odoo':
+    ensure => running,
+    enable => true,
   }
 }
